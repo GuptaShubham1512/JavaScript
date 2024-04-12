5af9ce63a74a7af9beeb2838d1154acec30f44c2
GuptaShubham1512 <148928391+GuptaShubham1512@users.noreply.github.com>
2024-04-12 23:19:18 +0530

    Add files via upload

---
+++ script3.js
@@ -0,0 +1,37 @@
+console.log("Iam a tutoral on loops")
+
+let a=1;
+//console.log(a)
+//aconsole.log(a+1)
+//aconsole.log(a+2)
+//aconsole.log(a+3)
+//for (let i = 0; i <100; i++) {
+  //  console.log(a+i);
+ //   
+//}
+    let obj = {
+         name: "Harry",
+         role: "Programmer",
+         company: "CodeWithHarry AI"
+     }
+     for(const key in obj){
+        console.log(key)
+     }
+
+ //let i = 5998;
+ //while (i<60000) {
+   //  console.log(i)
+     //i++;
+ //}
+
+ //let i = 2;  //important //result 2,3,4,5
+ //do {
+   //  console.log(i)
+     //i++;
+ //} while (i<6);
+
+ let i = 10;  //important //result 10
+ do {
+     console.log(i)
+     i++;
+ } while (i<6);
